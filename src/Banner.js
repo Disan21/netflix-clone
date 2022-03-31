f6109c844ab756589ed231ebe92e2bf2322469a5
Disan Davis <disandavis1999@gmail.com>
2022-03-31 10:59:59 +0530

    clone

---
+++ src/Banner.js
@@ -0,0 +1,51 @@
+import React, {useState,useEffect} from 'react'
+import instance from './instance'
+import requests from './requests'
+import './Banner.css'
+import axios from 'axios'
+const base_url="https://image.tmdb.org/t/p/original/"
+
+function Banner() {
+   const[movie,setMovie]=useState([])
+   useEffect(() =>{
+    async function fetchMovie(){
+        const request = await instance.get(requests.fetchNetflixOriginals)
+        setMovie(request.data.results[Math.floor(Math.random()* request.data.results.length -1)])
+         return request;
+    }
+    fetchMovie()
+   },[])
+   function truncate(str,n){
+       return str?.length >n ?str.substr(0 , n-1) + "....." : str;
+   }
+
+return (
+    <header className="banner"
+     style={{
+         backgroundSize:"cover",
+         backgroundImage:`url(${base_url}${movie?.backdrop_path})`,
+         backgroundPosition:"center"
+     }}
+    >
+        <div className="banner__contents">
+            <h1 className="banner_title">
+                {movie?.title || movie?.name || movie?.original_name}
+
+            </h1>
+            <div className="banner_buttons">
+                <button className="banner_button">Play</button>
+                <button className="banner_button">More Info</button>
+            </div>
+            <h1 className="banner_description">
+                {truncate(movie?.overview,150)}
+            </h1>
+
+        </div>
+        <div className="banner_fade"></div>
+
+
+    </header>
+  )
+}
+
+export default Banner
